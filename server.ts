82dd6b8e5ff7f13d9f4c1eb88726fffb0768e1f2
Shah Mohammad Mohibbullah Shovon <mohibbullahshovon.dev@gmail.com>
2026-07-01 10:16:13 +0600

    initial commit express app setup

---
+++ server.ts
@@ -0,0 +1,13 @@
+import {type Request,type Response,type NextFunction} from 'express'
+import app from "./src/app.js";
+
+const port = process.env.PORT || 3000;
+
+
+app.get('/', (req:Request,res:Response,next:NextFunction)=>{
+    res.send({message: "Welcome to elib api for ebook store" })
+});
+
+app.listen(port,()=>{
+    console.log("The server is litening at port:", port)
+});
