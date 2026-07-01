8a7ac5601d5689351eed9ed8eee2fb995c37847c
Shah Mohammad Mohibbullah Shovon <mohibbullahshovon.dev@gmail.com>
2026-07-01 10:43:31 +0600

    setup prettier code formater

--- server.ts
+++ server.ts
@@ -1,13 +1,16 @@
-import {type Request,type Response,type NextFunction} from 'express'
-import app from "./src/app.js";
+import { type Request, type Response, type NextFunction } from 'express';
+import app from './src/app.js';
 
 const port = process.env.PORT || 3000;
 
+function testing() {
+  console.log('testing');
+}
 
-app.get('/', (req:Request,res:Response,next:NextFunction)=>{
-    res.send({message: "Welcome to elib api for ebook store" })
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send({ message: 'Welcome to elib api for ebook store' });
 });
 
-app.listen(port,()=>{
-    console.log("The server is litening at port:", port)
+app.listen(port, () => {
+  console.log('The server is litening at port:', port);
 });
