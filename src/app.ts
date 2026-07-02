c1d835ed6162833c9d36bbdaccae9d6f6a386725
Shah Mohammad Mohibbullah Shovon <mohibbullahshovon.dev@gmail.com>
2026-07-02 11:55:46 +0600

    added user creation and user router for registration

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import globalErrorHandler from './middlewares/globalErrorHandler.js';
+import userRouter from './users/userRouter.js';
 
 const app = express();
 
@@ -7,6 +8,9 @@ const app = express();
 app.get('/', (req, res, next) => {
   res.json({ message: 'Welcome to elib api for ebook store' });
 });
+
+// user-router
+app.use('/api/users', userRouter);
 
 // global error handler
 app.use(globalErrorHandler);
