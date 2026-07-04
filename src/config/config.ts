8e2a8ef05aa9d28bcbd948926672b9789559ecae
Shah Mohammad Mohibbullah Shovon <mohibbullahshovon.dev@gmail.com>
2026-07-04 22:08:24 +0600

    configure cloudinary halfway

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,11 +3,14 @@ import { config as conf } from 'dotenv';
 conf();
 
 const _config = {
-  port: process.env.PORT || 3000,
-  mongodbURI: process.env.MONGODB_URI,
-  dbName: process.env.MONGODB_NAME,
-  env: process.env.NODE_ENV,
-  jwtSecret: process.env.JWT_SECRET,
+  port: process.env.PORT || 3000!,
+  mongodbURI: process.env.MONGODB_URI!,
+  dbName: process.env.MONGODB_NAME!,
+  env: process.env.NODE_ENV!,
+  jwtSecret: process.env.JWT_SECRET!,
+  cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME!,
+  cloudinaryApiKey: process.env.CLOUDINARY_API_KEY!,
+  cloudinarySecret: process.env.CLOUDINARY_SECRET!,
 };
 
 export const config = Object.freeze(_config);
